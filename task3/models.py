de7b42a3ed6482e3f64347ecc0222408b466acb3
Viktor <darkin@darkin.com>
2021-03-06 12:09:12 +0200

    Task 3

--- task3/models.py
+++ task3/models.py
@@ -1,7 +1,10 @@
+from datetime import timedelta
 from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
+from dateutil.relativedelta import relativedelta
 
 
 class Article(models.Model):
@@ -18,9 +21,16 @@ class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
     text = models.TextField(blank=True, null=True)
     parent = models.ForeignKey('self', null=True, blank=True, related_name="replies", on_delete=models.CASCADE)
+    date_created = models.DateTimeField()
 
     def __str__(self):
         return self.text
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.date_created = timezone.now() - relativedelta(years=1)
+        super().save(force_insert=False, force_update=False, using=None,
+             update_fields=None)
 
 
 class Like(models.Model):
